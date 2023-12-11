178a1ec303c14ea06503e97a150f860e479d8067
Bradym <4667076+Bradymck@users.noreply.github.com>
2023-12-10 23:41:46 -0800

    Refactor generate_prompt method to include Weaviate context

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,12 +1,20 @@
 class PromptGenerator:
+    def __init__(self):
+        self.weaviate_client = WeaviateClient()
+
     @staticmethod
-    def generate_prompt(static_instruction, os_section, ram_section, system_section, hdd_section, user_input_section):
+    def generate_prompt(weaviate_context, static_instruction, os_section, ram_section, system_section, hdd_section, user_input_section):
+            
+        weaviate_data = weaviate_context.get_context()
+        weaviate_section = f"Weaviate Context: {weaviate_data['text']}"
+
         prompt = (
-            f"{static_instruction}\n💾"
-            f"{os_section}\n💻"
-            f"This is the past 5 messages from the user:\n{ram_section}\n🗜"
-            f"{system_section}\n📂"
-            f"{hdd_section}\n💽"
-            f"{user_input_section}\n💬"
-        )
+                f"{static_instruction}\n💾"
+                f"{os_section}\n💻"
+                f"This is the past 5 messages from the user:\n{ram_section}\n🗜"
+                f"This is the past 5 messages from everyone including bots in chat:\n{system_section}\n📂"
+                f"{hdd_section}\n💽"
+                f"{user_input_section}\n💬"
+            )
+        
         return prompt
